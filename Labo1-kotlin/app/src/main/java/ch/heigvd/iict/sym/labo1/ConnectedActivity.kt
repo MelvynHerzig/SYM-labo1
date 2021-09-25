57f37cb0f43686613b716c62349804e2bb1e451a
MelvynHerzig <melvyn.herzig@heig-vd.ch>
2021-09-25 15:08:23 +0200

    permissions simples

--- Labo1-kotlin/app/src/main/java/ch/heigvd/iict/sym/labo1/ConnectedActivity.kt
+++ Labo1-kotlin/app/src/main/java/ch/heigvd/iict/sym/labo1/ConnectedActivity.kt
@@ -2,11 +2,14 @@ package ch.heigvd.iict.sym.labo1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
+import ch.heigvd.iict.sym.labo1.network.ImageDownloader
 
 class ConnectedActivity : AppCompatActivity() {
 
     private lateinit var email: TextView
+    private lateinit var profilePicture: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,8 +18,12 @@ class ConnectedActivity : AppCompatActivity() {
         // Récupération de l'intention et récupération de la chaîne
         val emailInput = intent.getStringExtra(EXTRA_EMAIL)
 
-        // Récupération du champ et mise à jour.
+        // Récupération du champ TextView et mise à jour.
         email = findViewById(R.id.connected_email_greetings)
         email.setText(emailInput)
+
+        // Récupération de l'image ImageView et mise à jour.
+        profilePicture = findViewById(R.id.connected_logo)
+        ImageDownloader(profilePicture, "https://thispersondoesnotexist.com/image").show()
     }
 }
